dd85e395bd86c369a2430f9a8fefbc870920fee0
王昱鑫 <wangyuxin0623@emoney.cn>
2018-04-02 19:03:44 +0800

    new article

--- pelicanconf.py
+++ pelicanconf.py
@@ -63,3 +63,7 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
+PLUGIN_PATHS = ['pelican-plugins']
+PLUGINS = ['i18n_subsites', 'extract_toc']
