1e90e64148ed93bf12b027921a9447f13b66a72b
lostsummer <lostsummer@gmail.com>
2024-01-17 17:28:43 +0800

    update theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,28 +8,38 @@ SITESUBTITLE = u'code less talk more :)'
 SITEURL = ''
 
 TIMEZONE = 'Asia/Shanghai'
-DEFAULT_LANG = u'zh'
+DEFAULT_LANG = u'en'
+CC_LICENSE = "CC-BY-NC-SA"
 
 PATH = 'content'
 PLUGIN_PATHS = ['plugins']
 STATIC_PATHS = ['images', 'extra']
 FAVICON = 'images/favicon.ico'
-PLUGINS = ['pelican-toc']
+PLUGINS = ['pelican-toc', 'i18n_subsites']
 THEME = 'themes/pelican-twitchy'
+BOOTSTRAP_THEME = 'journal'
+# THEME = 'themes/pelican-bootstrap3'
+# JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
+# I18N_TEMPLATES_LANG = 'en'
+
+TYPOGRIFY = True
+
 CHECK_MODIFIED_METHOD = "mtime"
+
 DISPLAY_TAGS_ON_MENU = True
 DISPLAY_PAGES_ON_MENU = True
 DISPLAY_RECENT_POSTS_ON_MENU = True
-EXPAND_LATEST_ON_INDEX = True
+
 ARTICLE_URL = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}.html'
 ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%b}/{date:%d}/{slug}.html'
 PAGE_URL = 'pages/{slug}.html'
 PAGE_SAVE_AS = 'pages/{slug}.html'
 NEWEST_FIRST_ARCHIVES = True
 RESPONSIVE_IMAGES = True
-CC_LICENSE = "CC-BY-NC-SA"
-SUMMARY_MAX_LENGTH = 0
 
+#SUMMARY_MAX_LENGTH = 20
+EXPAND_LATEST_ON_INDEX = 1
+RECENT_POST_COUNT = 5
 
 
 # Feed generation is usually not desired when developing
@@ -49,10 +59,9 @@ LINKS = (
         )
 
 # Code syntax
-PYGMENTS_STYLE = 'autumn'
-BOOTSTRAP_THEME = 'flatly'
-SHARE = True
-CUSTOM_CSS = "extra/custom.css"
+PYGMENTS_STYLE = 'emacs'
+
+#CUSTOM_CSS = "extra/custom.css"
 
 # Social widget
 SOCIAL = (('github', 'https://github.com/lostsummer'),
@@ -63,20 +72,19 @@ SOCIAL = (('github', 'https://github.com/lostsummer'),
 
 DEFAULT_PAGINATION = False
 
-TYPOGRIFY = True
+SHARE = True
 
-MARKDOWN = {
-    'extension_configs': {
-        'markdown.extensions.codehilite': {
-            'css_class': 'highlight',
-            'linenums': False
-        },
-        'markdown.extensions.extra': {},
-        'markdown.extensions.fenced_code': {},
-        'markdown.extensions.toc': {
-            'anchorlink': True
-        },
-    }
+
+TOC = {
+    'TOC_HEADERS'       : '^h[1-6]', # What headers should be included in
+                                     # the generated toc
+                                     # Expected format is a regular expression
+
+    'TOC_RUN'           : 'true',    # Default value for toc generation,
+                                     # if it does not evaluate
+                                     # to 'true' no toc will be generated
+
+    'TOC_INCLUDE_TITLE': 'true',     # If 'true' include title in toc
 }
 
 # Uncomment following line if you want document-relative URLs when developing
